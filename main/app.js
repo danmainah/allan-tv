067b35eab9de3ceca26261a3e8f19ce2d085cc48
Danmainah <danielmainawaweru@gmail.com>
2023-05-27 11:57:55 +0300

    add the schema wiht users likes comments and posts  tables

--- main/app.js
+++ main/app.js
@@ -14,5 +14,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/', indexRouter)
 
 module.exports = app;
