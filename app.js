24ec75ab1ab8e03fcc9ee947795451cb22221f4c
Danmainah <danielmainawaweru@gmail.com>
2023-06-07 18:25:35 +0300

    Add usr authentication and login security

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const bodyParser = require('body-parser');
+const db = require('./connectdb');
+const UserModel = require('./models/user');
+
+
+
+require('./auth/auth');
+
+const routes = require('./routes/user');
+const secureRoute = require('./routes/profile');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/', routes);
+
+// Plug in the JWT strategy as a middleware so only verified users can access this route.
+app.use('/user', passport.authenticate('jwt', { session: false }), secureRoute);
+
+// Handle errors.
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({ error: err });
+});
+
+app.listen(3001, () => {
+  console.log('Server started.')
+});
+
+module.exports = app
--- main/routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-router.get('/api/hello', (req, res) => {
-	res.json('hello world')
-})
-
-module.exports = router
